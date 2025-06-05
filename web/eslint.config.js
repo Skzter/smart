74a76c3863aed6d15281a6364a3b4c46c8c00167
Vincent Götze <vincent.goetze@stud.htwk-leipzig.de>
2025-06-05 19:38:42 +0200

    feat(#13): more setup for formatting and linting

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -1,8 +1,29 @@
 // empfohlene config con typescript-eslint.io
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    {
+        ignores: [
+            "dist/",
+            "node_modules/",
+            "public/",
+            "src/assets/",
+            ".vscode/*",
+            "!.vscode/extensions.json",
+
+            "*.config.*",
+            "!eslint.config.js",
+
+            "index.html",
+            "package.json",
+            "pnpm-*",
+            "README.md",
+
+            "tsconfig.*",
+            "!tsconfig.json",
+        ],
+    },
 );
