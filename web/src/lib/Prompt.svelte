2f4fd544e328ad03899ec35b3627468fe72dbb36
Vincent Götze <vincent.goetze@stud.htwk-leipzig.de>
2025-06-05 22:02:56 +0200

    style(#13): only whitespace changes

--- web/src/lib/Prompt.svelte
+++ web/src/lib/Prompt.svelte
@@ -1,23 +1,22 @@
 <script lang="ts">
     import {Button, Input} from "flowbite-svelte";
     import axios from "axios";
-    // geht nicht 
-    const url = "http://localhost:8080/api/hello/"
-    function apiReq(){
-	const input = document.getElementById("user-msg") as HTMLInputElement
-	const msg = input.value
-	axios({
-	  method: 'get',
-	  url: url,
-	  //data: { message: msg }
-	})
-	  .then(function (response) {
-		console.log(response.data);
-	  });
-    };
+    // geht nicht
+    const url = "http://localhost:8080/api/hello/";
+    function apiReq() {
+        const input = document.getElementById("user-msg") as HTMLInputElement;
+        const msg = input.value;
+        axios({
+            method: "get",
+            url: url,
+            //data: { message: msg }
+        }).then(function (response) {
+            console.log(response.data);
+        });
+    }
 </script>
 
 <div class="flex">
-    <Input type="text" id="user-msg" placeholder="Prompt" required/>
+    <Input type="text" id="user-msg" placeholder="Prompt" required />
     <Button color="purple" onclick={apiReq}>Send</Button>
 </div>
