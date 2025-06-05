e1b43cb2073381e093b6716460014cd414e10cc3
Viet Pham <viet.pham@stud.htwk-leipzig.de>
2025-06-05 20:31:53 +0200

    feat(#13): bot-message component prototype with prop

--- web/src/lib/BotMessage.svelte
+++ web/src/lib/BotMessage.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-    let message = 'Der Frühling ist eine wunderschöne Jahreszeit. Die Blumen beginnen zu blühen, die Vögel singen fröhlich, und die Tage werden länger und wärmer. Viele Menschen nutzen diese Zeit, um mehr Zeit im Freien zu verbringen, sei es beim Spaziergang im Park oder beim Gärtnern im eigenen Garten. Es ist eine Zeit des Neuanfangs und der Erneuerung, die positive Stimmung und Lebensfreude mit sich bringt.';
+    let props = $props();
 </script>
 
 <div class=" p-3 mr-10 w-1/2">
     <p class="p-1">Bot answering: </p>
-    <div class="p-1 border-2 border-double bg-gray-200">
-        <p class="font-mono">{message}</p>
+    <div class="p-3 border-2 border-double rounded-2xl bg-gray-200">
+        <p class="font-mono">{props.message}</p>
     </div>
 </div>
