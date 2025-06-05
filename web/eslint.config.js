d8a95cde960cce68e6be9fade2be6517754f5a9a
Vincent Götze <vincent.goetze@stud.htwk-leipzig.de>
2025-06-05 18:45:50 +0200

    feat(#13): inital frontend setup

---
+++ web/eslint.config.js
@@ -0,0 +1,8 @@
+// empfohlene config con typescript-eslint.io
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+);
