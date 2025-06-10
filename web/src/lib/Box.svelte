6739288dfa73fd54ee9e9b2e2a3579f9d15600a6
Viet Pham <viet.pham@stud.htwk-leipzig.de>
2025-06-10 16:48:12 +0200

    feat(#13): added MessageBox Layout

--- web/src/lib/Box.svelte
+++ web/src/lib/Box.svelte
@@ -3,19 +3,28 @@
     let text_align = $state("");
     let bg_col = $state("");
     let padding = $state("");
-
+    let width = $state("");
+    let direction = $state("");
     if (name == "User") {
         text_align = "text-end";
         bg_col = "bg-sky-300";
-        padding = "pr-5";
+        padding = "p-2";
+        width = "w-fit";
+        direction = "justify-end";
     } else if (name === "Bot") {
         text_align = "text-start";
         bg_col = "bg-gray-200";
-        padding = "pl-5";
+        padding = "p-2";
+        width = "w-fit";
+        direction = "justify-start";
     }
 </script>
 
-<div class="{bg_col} border-2 border-black border-solid rounded-xl">
-    <h1 class="{text_align} {padding} text-xl">{name}</h1>
-    <p class="{text_align} {padding}">{msg}</p>
+<div class = "flex {direction}">
+    <div class="{bg_col} {width} {direction} border-2 border-black border-solid rounded-xl">
+        <h1 class="{text_align} {padding} text-xl">{name}</h1>
+        <p class="{text_align} {padding}">{msg}</p>
+    </div>
 </div>
+
+
