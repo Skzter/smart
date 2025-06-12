cde15762a0e25467cd2a108b990ec8cd11b4d5cb
Thai Duong Pham Le <thai_duong_pham.le@stud.htwk-leipzig.de>
2025-06-12 19:08:50 +0200

    build(#18): add hello world route and deployment step to gitlab ci config

--- cmd/autotester/main.go
+++ cmd/autotester/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		//nolint:errcheck
+		fmt.Fprintf(w, "Hello World!")
+	})
+
+	//nolint:gosec
+	if err := http.ListenAndServe(":8081", mux); err != nil {
+		panic(err)
+	}
 }
