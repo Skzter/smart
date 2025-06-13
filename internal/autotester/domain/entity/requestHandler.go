20c997dac83422f9fdbeb7cbfa87447e00a7c539
Toni Nguyen <toni.nguyen@stud.htwk-leipzig.de>
2025-06-13 17:54:07 +0200

    feat(#29): Implement all route functionalities This commit introduces dummies of all route-related features.
    Co-authored-by: Lucas Stramke lucas.stramke@stud.htwk-leipzig.de
    Co-authored-by: Viet Pham viet.pham@stud.htwk-leipzig.de
    Co-authored-by: Henry Nguyen henry.nguyen@stud.htwk-leipzig.de
    Co-authored-by: Linh Nguyen linh.nguyen@stud.htwk-leipzig.de

--- internal/autotester/domain/entity/requestHandler.go
+++ internal/autotester/domain/entity/requestHandler.go
@@ -20,10 +20,28 @@ type ChatRequest struct {
 
 type ChatResponse struct {
 	Content    ChatRequest `json:"content"`
-	tools_call ToolCall
+	Tools_call ToolCall    `json:"tool_call"`
 }
 
 type ToolCall struct {
 	Name      string                 `json:"name"`
 	Arguments map[string]interface{} `json:"arguments"`
+}
+
+type ErrorMessage struct {
+	Error string `json:"message"`
+}
+
+type Session struct {
+	ConversationID string    `json:"ConversationId"`
+	Messages       []Message `json:"messages"`
+}
+
+type UserResponse struct {
+	UserID   string    `json:"userId"`
+	Sessions []Session `json:"allConversations"`
+}
+
+type UserRequest struct {
+	UserID string `json:"userId"`
 }
--- internal/autotester/domain/handler/request_handler.go
+++ internal/autotester/domain/handler/request_handler.go
@@ -1,31 +1,49 @@
-package handler
+package main
 
 import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
 	"gitlab.dit.htwk-leipzig.de/projekt2025-w-llm-unterstuetztes-autotesting-fuer-moderne-web-frontends/smart/internal/autotester/domain/entity"
 )
 
 // handle Frontend Request JSON to String
-func frontendToBackend(c *gin.Context) {
+func HandleChatRequest(c *gin.Context) {
 	var body entity.ChatRequest
-
 	if err := c.BindJSON(&body); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, entity.ErrorMessage{Error: "Bad Request"})
 		return
 	}
-	c.IndentedJSON(http.StatusCreated, body)
+
+	// validation Method
 
 	// Call Request to openAI
-	NewRequest(body.Message, body.ConversationID)
+	// service needed
+	// entity.Request(body.Message, body.ConversationID)
+
+	// handling response openai
+
+	// respons to frontend
+	c.IndentedJSON(http.StatusOK, entity.ChatResponse{Content: body})
 }
 
-func backendResponse(c *gin.Context) {
-
+func HandleUserInfoRequest(c *gin.Context) {
+	var body entity.UserRequest
+	if err := c.BindJSON(&body); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, entity.ErrorMessage{Error: "Bad Request"})
+		return
+	}
+	c.IndentedJSON(http.StatusOK, entity.UserResponse{UserID: body.UserID, Sessions: nil})
 }
 
 func main() {
 	router := gin.Default()
-	router.POST("/api/v1/chat", frontendToBackend)
-	router.Run("localhost:8080")
+	router.POST("/api/v1/chat", HandleChatRequest)
+	router.POST("/api/v1/userInfo", HandleUserInfoRequest)
+	err := router.Run("localhost:8080")
+
+	if err != nil {
+		return
+	}
 }
