5868e2a327e6d9c466d4abe209def3f8dc8399e1
Franca Lenz <franca.lenz@stud.htwk-leipzig.de>
2025-06-17 14:37:50 +0200

    feat(#25): path to api added

--- internal/suproxy/domain/handler/SuproxyAPI.go
+++ internal/suproxy/domain/handler/SuproxyAPI.go
@@ -10,14 +10,14 @@ import (
 func main() {
 	router := gin.Default()
 
-	router.GET("/", homepage)
+	router.POST("/api/v1/Offerlist", getOfferlist)
 
-	err := router.Run()
+	err := router.Run("localhost:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func homepage(c *gin.Context) {
-	c.String(http.StatusOK, "This is my Homepage")
+func getOfferlist(c *gin.Context) {
+	c.String(http.StatusOK, "This is my Offerlist")
 }
