39e3b19cb7c7d0cd19f6c5de397f0f78ae4b912c
Vincent Götze <vincent.goetze@stud.htwk-leipzig.de>
2025-06-18 18:30:43 +0200

    style(#13): changed some css

--- web/src/lib/Prompt.svelte
+++ web/src/lib/Prompt.svelte
@@ -3,9 +3,6 @@
     let { input = $bindable(""), onclick } = $props();
 
     function handleKeyPress(e: KeyboardEvent) {
-        console.log(e);
-        console.log(e.key);
-        console.log(e.shiftKey);
         if (e.key === "Enter" && input && !e.shiftKey) {
             onclick();
             input = "";
@@ -15,7 +12,7 @@
 </script>
 
 <div
-    class="fixed bottom-0 left-0 w-full flex flex-row w-screen bg-white p-4 border-t gap-2"
+    class="fixed bottom-0 left-0 w-full flex flex-row w-screen items-center bg-white p-4 border-t gap-2"
     on:keydown={handleKeyPress}
 >
     <textarea
@@ -23,9 +20,9 @@
         placeholder="Prompt"
         required
         class="w-9/10 resize-none overflow-y-auto"
-        rows={1}
+        rows={3}
     />
-    <Button color="purple" class="w-1/10" {onclick} disabled={!input}
+    <Button color="purple" class="w-1/10 h-1/3" {onclick} disabled={!input}
         >Send</Button
     >
 </div>
