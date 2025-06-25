eba5b2f4763b41691923da555ba81361b27f2556
Toni Nguyen <toni.nguyen@stud.htwk-leipzig.de>
2025-06-25 17:05:20 +0200

    refactor(#47): • Outsourcing router from main • Changed strings to const
    Co-authored-by: Lucas Stramke <lucas.stramke@stud.htwk-leipzig.de>

--- cmd/autotester/main.go
+++ cmd/autotester/main.go
@@ -1,42 +1,7 @@
 package main
 
-import (
-	"io/fs"
-	"log/slog"
-	"net/http"
-	"os"
-
-	"github.com/gin-gonic/gin"
-
-	"gitlab.dit.htwk-leipzig.de/projekt2025-w-llm-unterstuetztes-autotesting-fuer-moderne-web-frontends/smart/internal/autotester/domain/handler"
-	"gitlab.dit.htwk-leipzig.de/projekt2025-w-llm-unterstuetztes-autotesting-fuer-moderne-web-frontends/smart/web"
-)
+import "gitlab.dit.htwk-leipzig.de/projekt2025-w-llm-unterstuetztes-autotesting-fuer-moderne-web-frontends/smart/internal/autotester/application"
 
 func main() {
-	router := gin.Default()
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
-	controller, err := handler.NewAutotesterController(logger)
-
-	if err != nil {
-		logger.Error(err.Error())
-		return
-	}
-
-	router.POST("/api/v1/chat", func(c *gin.Context) {
-		controller.HandleChatRequest(c)
-	})
-
-	staticFS, err := fs.Sub(web.DistFS, "dist")
-	if err != nil {
-		logger.Error(err.Error())
-		return
-	}
-
-	router.StaticFS("/", http.FS(staticFS))
-
-	err = router.Run(":8081")
-	if err != nil {
-		logger.Error(err.Error())
-		return
-	}
+	application.SetRoutes()
 }
---
+++ internal/autotester/application/router.go
@@ -0,0 +1,46 @@
+package application
+
+import (
+	"io/fs"
+	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+
+	"gitlab.dit.htwk-leipzig.de/projekt2025-w-llm-unterstuetztes-autotesting-fuer-moderne-web-frontends/smart/internal/autotester/domain/handler"
+	"gitlab.dit.htwk-leipzig.de/projekt2025-w-llm-unterstuetztes-autotesting-fuer-moderne-web-frontends/smart/web"
+)
+
+// Initializes the HTTP server, sets up API routes and serves static files.
+// Registers endpointa and serves frontend assets from the embedded dist directory.
+func SetRoutes() {
+	router := gin.Default()
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	controller, err := handler.NewAutotesterController(logger)
+	const chatApiEndpoint string = "/api/v1/chat"
+	const serverPort string = ":8081"
+
+	if err != nil {
+		logger.Error(err.Error())
+		return
+	}
+
+	router.POST(chatApiEndpoint, func(c *gin.Context) {
+		controller.HandleChatRequest(c)
+	})
+
+	staticFS, err := fs.Sub(web.DistFS, "dist")
+	if err != nil {
+		logger.Error(err.Error())
+		return
+	}
+
+	router.StaticFS("/", http.FS(staticFS))
+
+	err = router.Run(serverPort)
+	if err != nil {
+		logger.Error(err.Error())
+		return
+	}
+}
