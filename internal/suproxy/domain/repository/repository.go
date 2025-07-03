3a4b13e3714cda68230fcc8e0121e4ac8211ac64
Max Kohla <max.kohla@stud.htwk-leipzig.de>
2025-07-03 20:23:03 +0200

    feat(#55): add generateKey Co-Author Franca Lenz

--- internal/suproxy/domain/repository/repository.go
+++ internal/suproxy/domain/repository/repository.go
@@ -1,8 +1,11 @@
 package repository
 
 import (
+	"context"
 	"fmt"
 	"log/slog"
+	"reflect"
+	"time"
 
 	"gitlab.dit.htwk-leipzig.de/projekt2025-w-llm-unterstuetztes-autotesting-fuer-moderne-web-frontends/smart/internal/build"
 	wrapperEntity "gitlab.dit.htwk-leipzig.de/projekt2025-w-llm-unterstuetztes-autotesting-fuer-moderne-web-frontends/smart/internal/shared/domain/entity/wrapper"
@@ -13,13 +16,13 @@ import (
 
 type DatabaseRepository interface {
 	// creates a request into the database
-	CreateRequest(entity.DatabaseEntry) error
-	// gets Request by ?
-	ReadRequest(string) (entity.DatabaseEntry, error)
-	// changes the content of a request
-	UpdateRequest(string, entity.DatabaseEntry) error
-	// deletes a request from the database
-	DeleteRequest(string) error
+	CreateRequest(ctx context.Context, dbEntry entity.DatabaseEntry) error
+	// gets Request (access through key)
+	ReadRequest(ctx context.Context, key string) (*entity.DatabaseEntry, error)
+	// changes the content of a request (access through key)
+	UpdateRequest(ctx context.Context, key string, dbEntry entity.DatabaseEntry) error
+	// deletes a request from the database (access through key)
+	DeleteRequest(ctx context.Context, key string) error
 }
 
 type databaseRepository struct {
@@ -58,46 +61,72 @@ func NewDatabaseRepository(logger *slog.Logger) (DatabaseRepository, error) {
 }
 
 // CreateRequest creates, converts JSON to parquet and uploads request to database
-func (dbR *databaseRepository) CreateRequest(dbEntry entity.DatabaseEntry) error {
+func (dbR *databaseRepository) CreateRequest(ctx context.Context, dbEntry entity.DatabaseEntry) error {
 	if err := validateDbEntry(dbEntry, dbR); err != nil {
-		dbR.logger.Error("Failed to validate dbEntry", slog.String("error", err.Error()))
+		dbR.logger.Error("failed to validate dbEntry", slog.String("error", err.Error()))
 	}
 
 	parquetData, err := dbR.parquetWrapper.WriteStructToParquet(dbEntry)
 	if err != nil {
-		dbR.logger.Error("Failed to write parquet", slog.String("error", err.Error()))
+		dbR.logger.Error("failed to write parquet", slog.String("error", err.Error()))
 		return err
 	}
 
 	dbR.logger.Info("Parquet data created", slog.Int("size_bytes", len(parquetData)))
 
+	metadata := map[string]string{
+		"created": string(rune(time.Now().Unix())),
+	}
+
+	// struct-unixtimestamp
+	key := generateKey(string(reflect.TypeOf(dbEntry).Name()), metadata["created"])
+
+	// Upload file (automatically adds .parquet extension if missing)
+	err = dbR.s3Wrapper.UploadParquetFile(ctx, key, parquetData, metadata)
+	if err != nil {
+		dbR.logger.Error("Upload failed", slog.String("error", err.Error()))
+		return err
+	}
+
 	return nil
 }
 
-func (dbR *databaseRepository) ReadRequest(string) (entity.DatabaseEntry, error) {
-	return entity.DatabaseEntry{}, nil
+func (dbR *databaseRepository) ReadRequest(ctx context.Context, key string) (*entity.DatabaseEntry, error) {
+	// Download file
+	data, metadata, err := dbR.s3Wrapper.DownloadParquetFile(ctx, "data/2024/01/user-events.parquet")
+	if err != nil {
+		dbR.logger.Error("Download failed", slog.String("error", err.Error()))
+		return nil, err
+	}
+
+	dbR.logger.Info("File downloaded",
+		slog.Int("size", len(data)),
+		slog.Any("metadata", metadata),
+	)
+
+	return &entity.DatabaseEntry{}, nil
 }
 
-func (dbR *databaseRepository) UpdateRequest(string, entity.DatabaseEntry) error {
+func (dbR *databaseRepository) UpdateRequest(ctx context.Context, key string, dbEntry entity.DatabaseEntry) error {
 	return nil
 }
 
-func (dbR *databaseRepository) DeleteRequest(string) error {
+func (dbR *databaseRepository) DeleteRequest(ctx context.Context, key string) error {
 	return nil
 }
 
 // validateDbEntry validates dbEntry
 func validateDbEntry(dbEntry entity.DatabaseEntry, dbR *databaseRepository) error {
 	if err := validateRequest(dbEntry.Request); err != nil {
-		dbR.logger.Error("Failed to validate request", slog.String("error", err.Error()))
+		dbR.logger.Error("failed to validate request", slog.String("error", err.Error()))
 		return err
 	}
 	if err := validateResponse(dbEntry.Response); err != nil {
-		dbR.logger.Error("Failed to validate response", slog.String("error", err.Error()))
+		dbR.logger.Error("failed to validate response", slog.String("error", err.Error()))
 		return err
 	}
 	if err := validateTags(dbEntry.Tags); err != nil {
-		dbR.logger.Error("Failed to validate tags", slog.String("error", err.Error()))
+		dbR.logger.Error("failed to validate tags", slog.String("error", err.Error()))
 		return err
 	}
 	return nil
@@ -137,4 +166,8 @@ func validateTags(t []string) error {
 		return fmt.Errorf("tags must not be empty")
 	}
 	return nil
+}
+
+func generateKey(structName string, unixTimestamp string) string {
+	return structName + "-" + unixTimestamp
 }
