cc4a9202b9faefba2cf65162eeb727c6f27baa49
Vincent Götze <vincent.goetze@stud.htwk-leipzig.de>
2025-07-04 00:35:09 +0200

    test(#40): first basic tests

--- internal/shared/domain/repository/openaiRepository_test.go
+++ internal/shared/domain/repository/openaiRepository_test.go
@@ -1,85 +1,91 @@
 package repository
 
 import (
+	"context"
 	"log/slog"
 	"testing"
 
 	"gitlab.dit.htwk-leipzig.de/projekt2025-w-llm-unterstuetztes-autotesting-fuer-moderne-web-frontends/smart/internal/shared/domain/entity"
 )
 
-func TestRepository(t *testing.T) {
+func TestOpenaiRepository_NewOpenAiRepo(t *testing.T) {
 	logger := slog.New(slog.DiscardHandler)
-
-	// test with invalid parameters
-	if _, err := NewOpenAiRepository(nil, 0); err == nil {
-		t.Error("called NewOpenAiRepository with invalid parmeters, but no error was thrown")
-		return
-	}
-
-	// test with proper parameters
-	repo, err := NewOpenAiRepository(logger, 5)
-	if err != nil || repo == nil {
-		t.Errorf("failed to create OpenAiRepository: %v", err)
-		return
-	}
-
-	// test with missing parameters
-	// point of this test is to pass invalid value as ctx parameter, but prevented by linter
-	if _, err := repo.CreateRequest(t.Context(), entity.Request{}); err == nil {
-		t.Error("created request with invalid parmeters, but no error was thrown")
-		return
-	}
-
-	/*
-		// test with proper parameters
-		resp, err := repo.CreateRequest(t.Context(), entity.Request{Prompt: "What is the capital of France?", SessionID: ""}, "You are a helpfull AI assistant", "gpt-4.1-nano-2025-04-14")
-		if err != nil || resp == nil {
-			t.Errorf("failed to create request: %v", err)
-			return
-		}
-
-		if resp.SessionID == "" {
-			t.Errorf("response contained no id")
-			return
-		}
-
-		if resp.Text == "" {
-			t.Errorf("response contained no text")
-			return
-		}
-	*/
-}
-
-/*
-type input struct {
-	timeout int
-	logger  *slog.Logger
-}
-
-func TestOpenaiRepository(t *testing.T) {
-	// need map with input, output of functions
-	// TODO: implement
-	// newOAIRepo(logger, timeout) returned nil, err, openAI struct
-	// createRequest(context, request entity) returned nil, err, response entity
-	testsNewOpenAiRepo := map[string]struct {
-		input  input
-		result any
+	tests := []struct {
+		name             string
+		logger           *slog.Logger
+		timeout          int
+		expectedOutcome  any // hier muss mal openAi repo hin
+		expectedError    bool
+		expectedErrorMsg string
 	}{
-		"failing to create new open ai repo": {
-			input:  "",
-			result: "",
+		{
+			name:             "creating repo with nil logger, correct timeout",
+			logger:           nil,
+			timeout:          5,
+			expectedOutcome:  nil,
+			expectedError:    true,
+			expectedErrorMsg: "assert failed: given value at index 0 is nil",
+		},
+		{
+			name:             "creating repo with negative timeout, correct logger",
+			logger:           logger,
+			timeout:          -1,
+			expectedOutcome:  nil,
+			expectedError:    true,
+			expectedErrorMsg: "invalid timout: -1 seconds",
 		},
 	}
 
-	// need testing logic
-		for name, test := range tests {
-		  // test := test // NOTE: uncomment for Go < 1.22, see /doc/faq#closures_and_goroutines
-		  t.Run(name, func(t *testing.T) {
-		    t.Parallel()
-		    if got, expected := reverse(test.input), test.result; got != expected { //wo reverse dann halt die zu testende Function ig
-		      t.Fatalf("reverse(%q) returned %q; expected %q", test.input, got, expected)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			if test.expectedError {
+				repo, err := NewOpenAiRepository(test.logger, test.timeout)
+				if repo != test.expectedOutcome && err.Error() != test.expectedErrorMsg {
+					t.Errorf("wanted: %q, got: %q", test.expectedOutcome, repo)
+				}
+			}
+		})
 	}
-		  })
-		}
 }
-*/
+
+func TestOpenaiRepository_CreateRequest(t *testing.T) {
+	// helper logger and repo
+	logger := slog.New(slog.DiscardHandler)
+	repo, _ := NewOpenAiRepository(logger, 5)
+
+	tests := []struct {
+		name             string
+		ctx              context.Context
+		request          entity.Request
+		expectedOutcome  *entity.Response
+		expectedError    bool
+		expectedErrorMsg string
+	}{
+		{
+			name: "create request with nil context, correct request entity",
+			ctx:  nil,
+			request: entity.Request{
+				Prompt:       "user prompt",
+				SessionID:    "123",
+				Model:        "nano",
+				SystemPrompt: "sys prompt",
+			},
+			expectedOutcome:  nil,
+			expectedError:    true,
+			expectedErrorMsg: "assert failed: given value at index 0 is nil",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			if test.expectedError {
+				resp, err := repo.CreateRequest(test.ctx, test.request)
+				if resp != test.expectedOutcome && err.Error() != test.expectedErrorMsg {
+					t.Errorf("wanted: %q, got: %q", test.expectedOutcome, resp)
+				}
+			}
+		})
+	}
+}
