c7147752f6a4bc144bb969e12837f74835eae2e3
Lev Gusiev <lev.gusiev@stud.htwk-leipzig.de>
2025-07-09 16:45:11 +0200

    feat(#59): Add a get function in historyService

--- internal/autotester/domain/service/historyStorageService.go
+++ internal/autotester/domain/service/historyStorageService.go
@@ -2,6 +2,17 @@ package service
 
 import "gitlab.dit.htwk-leipzig.de/projekt2025-w-llm-unterstuetztes-autotesting-fuer-moderne-web-frontends/smart/internal/autotester/domain/entity"
 
-type HistoryStorageServcie interface {
+type HistoryStorageService interface {
 	SaveHistory(entity.SessionSummary) error
+}
+type historyStorageService struct {
+}
+
+func NewHistoryStorageService() HistoryStorageService {
+	return &historyStorageService{}
+}
+
+// Die Methode, die vom Handler aufgerufen wird
+func (s *historyStorageService) SaveHistory(summary entity.SessionSummary) error {
+	return nil
 }
