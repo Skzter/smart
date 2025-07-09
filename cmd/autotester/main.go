1f5bedd0a5c88d2c9f61b9e9f5dc829e20e206f9
Toni Nguyen <toni.nguyen@stud.htwk-leipzig.de>
2025-07-09 14:51:44 +0200

    refactor(#47): deleted wrong config using and uneceesary error check

--- cmd/autotester/main.go
+++ cmd/autotester/main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"context"
-
 	"gitlab.dit.htwk-leipzig.de/projekt2025-w-llm-unterstuetztes-autotesting-fuer-moderne-web-frontends/smart/internal/autotester/application"
 	"gitlab.dit.htwk-leipzig.de/projekt2025-w-llm-unterstuetztes-autotesting-fuer-moderne-web-frontends/smart/internal/autotester/domain/config"
 )
 
 func main() {
-	cfg, err := config.LoadFromPath(context.Background(), "configs/autotester.pkl")
-
-	application.SetupRoutes(cfg)
-
+	cfg, err := config.LoadAppConfig()
 	if err != nil {
 		panic(err)
 	}
+
+	application.SetupRoutes(cfg)
 }
