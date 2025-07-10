8a46ca86f9d1811bbfee7bd5f8918fa77c88ab05
Lucas Stramke <lucas.stramke@stud.htwk-leipzig.de>
2025-07-10 17:34:32 +0200

    feat(#59): add implementation for saveTestCase, include testcases and adjustment in mockery.yml

--- internal/autotester/domain/service/testcaseStorageService.go
+++ internal/autotester/domain/service/testcaseStorageService.go
@@ -1,7 +1,31 @@
 package service
 
-import "gitlab.dit.htwk-leipzig.de/projekt2025-w-llm-unterstuetztes-autotesting-fuer-moderne-web-frontends/smart/internal/autotester/domain/entity"
+import (
+	"context"
+	"log/slog"
+
+	"gitlab.dit.htwk-leipzig.de/projekt2025-w-llm-unterstuetztes-autotesting-fuer-moderne-web-frontends/smart/internal/autotester/domain/entity"
+	"gitlab.dit.htwk-leipzig.de/projekt2025-w-llm-unterstuetztes-autotesting-fuer-moderne-web-frontends/smart/internal/autotester/domain/repository"
+	"gitlab.dit.htwk-leipzig.de/projekt2025-w-llm-unterstuetztes-autotesting-fuer-moderne-web-frontends/smart/internal/shared/lib/assert"
+)
 
 type TestcaseStorageService interface {
-	saveTestCase(entity.TestCase) error
+	SaveTestCase(context.Context, *entity.TestCase) error
+}
+
+type testcaseStorageService struct {
+	logger *slog.Logger
+	repo   repository.TestCaseStorageRepository
+}
+
+func NewTestcaseStorageService(logger *slog.Logger, repo repository.TestCaseStorageRepository) (TestcaseStorageService, error) {
+	if err := assert.NotNil(logger, repo); err != nil {
+		return nil, err
+	}
+
+	return &testcaseStorageService{logger: logger, repo: repo}, nil
+}
+
+func (t *testcaseStorageService) SaveTestCase(ctx context.Context, testCase *entity.TestCase) error {
+	return t.repo.Create(ctx, testCase)
 }
