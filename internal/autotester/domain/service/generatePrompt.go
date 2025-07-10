26011e39aca72064bd652c5c20321167beb7126e
Luca Schneider <luca.schneider@stud.htwk-leipzig.de>
2025-07-10 19:27:36 +0200

    test(#61): added unit test using mocks

--- internal/autotester/domain/service/generatePrompt.go
+++ internal/autotester/domain/service/generatePrompt.go
@@ -12,14 +12,14 @@ import (
 
 // GeneratePromptService provides functionality to generate test prompts using OpenAI.
 type GeneratePromptService struct {
-	service *service.OpenAIService
+	service service.OpenAIService
 	config  *config.Config
 	logger  *slog.Logger
 }
 
 // NewGeneratePromptService creates a new GeneratePromptService instance.
 // Returns an error if any required dependencies are nil.
-func NewGeneratePromptService(service *service.OpenAIService, config *config.Config, logger *slog.Logger) (*GeneratePromptService, error) {
+func NewGeneratePromptService(service service.OpenAIService, config *config.Config, logger *slog.Logger) (*GeneratePromptService, error) {
 	if err := assert.NotNil(service, config, logger); err != nil {
 		return nil, err
 	}
---
+++ internal/autotester/domain/service/generatePrompt_test.go
@@ -0,0 +1,65 @@
+package service
+
+import (
+	"log/slog"
+	"testing"
+
+	"gitlab.dit.htwk-leipzig.de/projekt2025-w-llm-unterstuetztes-autotesting-fuer-moderne-web-frontends/smart/internal/autotester/domain/config"
+	"gitlab.dit.htwk-leipzig.de/projekt2025-w-llm-unterstuetztes-autotesting-fuer-moderne-web-frontends/smart/internal/shared/domain/mocks"
+	srv "gitlab.dit.htwk-leipzig.de/projekt2025-w-llm-unterstuetztes-autotesting-fuer-moderne-web-frontends/smart/internal/shared/domain/service"
+)
+
+func TestNewGeneratePromptService(t *testing.T) {
+	service := &mocks.MockOpenAIService{}
+	logger := slog.Default()
+	cfg := config.Config{}
+
+	tests := []struct {
+		name    string
+		service srv.OpenAIService
+		config  *config.Config
+		logger  *slog.Logger
+		wantErr bool
+	}{
+		{
+			name:    "all not nil",
+			service: service,
+			config:  &cfg,
+			logger:  logger,
+			wantErr: false,
+		},
+		{
+			name:    "nil service",
+			service: nil,
+			config:  &cfg,
+			logger:  logger,
+			wantErr: true,
+		},
+		{
+			name:    "nil config",
+			service: service,
+			config:  nil,
+			logger:  logger,
+			wantErr: true,
+		},
+		{
+			name:    "nil logger",
+			service: service,
+			config:  &cfg,
+			logger:  nil,
+			wantErr: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			repo, err := NewGeneratePromptService(test.service, test.config, test.logger)
+			if (err != nil) != test.wantErr {
+				t.Errorf("NewGeneratePromptService() error = %v, wantErr %v", err, test.wantErr)
+			}
+			if !test.wantErr && repo == nil {
+				t.Errorf("NewGeneratePromptService() returned nil service")
+			}
+		})
+	}
+}
