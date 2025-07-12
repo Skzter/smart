2564f371a538dbd740c0403543c47e36f3c03b76
Johannes Kresner <johannes.kresner@stud.htwk-leipzig.de>
2025-07-12 19:27:19 +0200

    feat(#69): add a login ui and oauth0 auth handling

--- cmd/autotester/main.go
+++ cmd/autotester/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "gitlab.dit.htwk-leipzig.de/projekt2025-w-llm-unterstuetztes-autotesting-fuer-moderne-web-frontends/smart/internal/autotester/domain/config"
+import (
+	"os"
+
+	"gitlab.dit.htwk-leipzig.de/projekt2025-w-llm-unterstuetztes-autotesting-fuer-moderne-web-frontends/smart/internal/autotester/domain/config"
+	"gorm.io/gorm/logger"
+)
 
 func main() {
 	cfg, err := config.LoadConfig()
@@ -10,7 +15,8 @@ func main() {
 
 	router, err := InitializeApp(cfg)
 	if err != nil {
-		panic(err)
+		logger.Error("Failed to load app config", "error", err)
+		os.Exit(1)
 	}
 
 	if err := router.Run(":" + cfg.Port); err != nil {
