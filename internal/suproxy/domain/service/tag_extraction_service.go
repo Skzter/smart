7c9c684ab39961bb3b07eea5de56e14ba254ef13
Daria Gashkova <daria.gashkova@stud.htwk-leipzig.de>
2025-07-15 12:50:47 +0000

    (fix #62): fix tag search service, add constructor

--- internal/suproxy/domain/service/tag_extraction_service.go
+++ internal/suproxy/domain/service/tag_extraction_service.go
@@ -3,14 +3,35 @@ package service
 import (
 	"errors"
 	"fmt"
+	"log/slog"
 	"strings"
 )
 
-func KeywordTagExtractor(prompt string, tags []string) ([]string, error) {
+type TagExtractionService interface {
+	KeywordTagExtractor(prompt string, tags []string) ([]string, error)
+}
+
+// KeywordTagExtractor extracts tags from a given prompt based on a list of tags.
+
+type tagExtractionService struct {
+	logger *slog.Logger
+}
+
+func NewTagExtractionService(logger *slog.Logger) TagExtractionService {
+	return &tagExtractionService{
+		logger: logger,
+	}
+}
+
+func (k *tagExtractionService) KeywordTagExtractor(prompt string) ([]string, error) {
+
+	tags[] = dbService.GetAllTags()
+
 	if prompt == "" {
 		return nil, errors.New("empty prompt")
 	}
 
+	// Check if any existing tags are provided in the prompt
 	var extractedTags []string
 	for _, tag := range tags {
 		if strings.Contains(strings.ToLower(prompt), strings.ToLower(tag)) {
@@ -24,3 +45,4 @@ func KeywordTagExtractor(prompt string, tags []string) ([]string, error) {
 
 	return extractedTags, nil
 }
+
