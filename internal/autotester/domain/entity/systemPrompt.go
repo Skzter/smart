0680f4503469c0e4b5758508e071061e569c7548
Lev Gusiev <lev.gusiev@stud.htwk-leipzig.de>
2025-07-15 15:07:07 +0200

    fix(#50): Add some comments because of linter revive

--- internal/autotester/domain/entity/systemPrompt.go
+++ internal/autotester/domain/entity/systemPrompt.go
@@ -1,5 +1,6 @@
 package entity
 
+// SystemPrompt represents a system prompt for the autotester.
 // UserPrompt embeds Prompt.
 // Created to enforce type safety for RequestForLLM
 type SystemPrompt struct {
--- internal/build/autotesterConfig.go
+++ internal/build/autotesterConfig.go
@@ -2,5 +2,8 @@ package build
 
 import "embed"
 
+// AutotesterEmbedConfigs is an embedded filesystem containing the autotester configuration files.
+// It is used to load the autotester configuration at runtime.
+//
 //go:embed configs/autotester.msgpack
 var AutotesterEmbedConfigs embed.FS
