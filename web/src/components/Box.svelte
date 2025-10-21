54a8d4eebebccd5c3ee15fa4d34a262cfa878dd4
Franca Lenz <franca.lenz@stud.htwk-leipzig.de>
2025-10-21 20:00:31 +0200

    fix(#82): changed width from user to 75% and bot fit and overlow

--- web/src/components/Box.svelte
+++ web/src/components/Box.svelte
@@ -8,16 +8,17 @@
     class:justify-start={name === "Bot"}
 >
     <div
-        class="font-mono w-fit p-2.5 border-2 border-black border-solid rounded-xl"
-        class:justify-end={name === "User"}
-        class:justify-start={name === "Bot"}
+        class="font-mono p-2.5 border-2 border-black border-solid rounded-xl break-words"
+        class:w-[75%]={name === "User"}
+        class:w-fit={name === "Bot"}
         class:bg-sky-300={name === "User"}
         class:bg-gray-200={name === "Bot"}
     >
+
         <h1 class="tracking-wide uppercase font-bold text-xl">
             {name}
         </h1>
-        <p class="font-sans whitespace-pre-wrap">
+        <p class="font-sans whitespace-pre-wrap break-words">
             {msg}
         </p>
     </div>
