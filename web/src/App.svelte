584ab7ca9a67a047de807a348791b14fa22cb8d5
Vincent Götze <vincent.goetze@stud.htwk-leipzig.de>
2025-10-23 13:46:33 +0200

    feat(#98): frontend displays error from api response

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -10,7 +10,7 @@
 
     let prompt = $state("");
     let convo = $state<{ id: number; question: string; answer: string }[]>([]);
-    
+
     // get ConversationId for api calls from cookies
     // Note: We should ideally get this from a backend state associated with the user
     var conversationId = localStorage.getItem("conversationId") || "";
@@ -57,8 +57,12 @@
             convo[convo.length - 1].answer = answer.data.message.data;
             setConversationId(answer.data.conversationId);
         } catch (err) {
-            console.error("api call failed", err);
-            convo[convo.length - 1].answer = "Interner Server Error - Bitte nochmal versuchen!";
+            if (err.isAxiosError) {
+                convo[convo.length - 1].answer = err.response.data.message;
+            } else {
+                convo[convo.length - 1].answer =
+                    "no axios error returned - something went horribly wrong";
+            }
         } finally {
             isLoading = false;
         }
@@ -113,7 +117,9 @@
         <Prompt bind:input={prompt} {onclick} />
     </footer>
 {:else}
-    <main class="flex h-screen w-screen items-center justify-center bg-gray-100 dark:bg-gray-900">
+    <main
+        class="flex h-screen w-screen items-center justify-center bg-gray-100 dark:bg-gray-900"
+    >
         <div
             class="w-full max-w-sm rounded-lg bg-white p-8 text-center shadow-xl dark:bg-gray-800"
         >
