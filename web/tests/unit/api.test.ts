f5afb83861b94cf39ed497b0b771b43e27463cfe
Vincent Götze <vincent.goetze@stud.htwk-leipzig.de>
2025-11-10 08:58:39 +0100

    fix(#62): router group, frontend now according to spec

--- web/tests/unit/api.test.ts
+++ web/tests/unit/api.test.ts
@@ -40,7 +40,7 @@ describe("API Functions", () => {
             expect(mockedAxios).toHaveBeenCalledWith({
                 method: "post",
                 url: "/userInfo",
-                baseURL: "/api/v1/chat",
+                baseURL: "/api/v1",
                 data: mockUserParams,
             });
             expect(result.data).toEqual(mockResponseData);
@@ -68,7 +68,7 @@ describe("API Functions", () => {
             expect(mockedAxios).toHaveBeenCalledWith({
                 method: "post",
                 url: "/chat",
-                baseURL: "/api/v1/chat",
+                baseURL: "/api/v1",
                 data: mockChatParams,
             });
             expect(result.data).toEqual(mockResponseData);
@@ -112,7 +112,7 @@ describe("API Functions", () => {
             expect(mockedAxios).toHaveBeenCalledWith({
                 method: "get",
                 url: "/template",
-                baseURL: "/api/v1/chat",
+                baseURL: "/api/v1",
             });
             expect(result.data).toEqual(mockResponseData);
         });
