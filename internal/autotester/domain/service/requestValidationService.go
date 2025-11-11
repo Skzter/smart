ea6730f53df6eb6caad563af6135327d502e0d3f
Daria Gashkova <daria.gashkova@stud.htwk-leipzig.de>
2025-11-11 19:14:37 +0100

    feat(#116): define the service. Coauthor: @lev.gusiev

--- internal/autotester/domain/service/requestValidationService.go
+++ internal/autotester/domain/service/requestValidationService.go
@@ -1,15 +1,29 @@
 package service
 
-/*import (
+/*
+import (
+	"context"
+	//"fmt"
 
+	sharedEntity "gitlab.dit.htwk-leipzig.de/projekt2025-w-llm-unterstuetztes-autotesting-fuer-moderne-web-frontends/smart/internal/shared/domain/entity"
+	sharedService "gitlab.dit.htwk-leipzig.de/projekt2025-w-llm-unterstuetztes-autotesting-fuer-moderne-web-frontends/smart/internal/shared/domain/service"
 )
 
-
 type RequestValidationService interface {
-	ValidateRequest(ctx context.Context, request string, sessionID string) (bool, string, error)
+	ValidateRequest(ctx context.Context, req sharedEntity.Request) (bool, error)
 }
 
+type requestValidationService struct {
+	openAIService sharedService.OpenAI
+	req           sharedEntity.Request
+}
 
+func ValidateRequest(ctx context.Context, req sharedEntity.Request) (bool, error) {
+	// Implement request validation logic here
+	return true, nil
+}
+
+/*
  Files to connect:
 - internal/autotester/domain/service/requestValidationService.go
 - internal/shared/domain/repository/openaiRepository.go
