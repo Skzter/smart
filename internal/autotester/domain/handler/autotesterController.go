cc382d292c871bc6cf5bab90db252772bcff02f8
Lucas Stramke <lucas.stramke@stud.htwk-leipzig.de>
2025-11-20 21:46:04 +0100

    refactor(#129): extract handler into individual files for clarity
    Introduces new sections: chat, template, runTest, and localStorage

---
+++ internal/autotester/domain/handler/autotesterController.go
@@ -0,0 +1,41 @@
+package handler
+
+import (
+	"log/slog"
+
+	"gitlab.dit.htwk-leipzig.de/projekt2025-w-llm-unterstuetztes-autotesting-fuer-moderne-web-frontends/smart/internal/autotester/domain/config"
+	"gitlab.dit.htwk-leipzig.de/projekt2025-w-llm-unterstuetztes-autotesting-fuer-moderne-web-frontends/smart/internal/autotester/domain/service"
+	"gitlab.dit.htwk-leipzig.de/projekt2025-w-llm-unterstuetztes-autotesting-fuer-moderne-web-frontends/smart/internal/shared/lib/assert"
+)
+
+// AutotesterController is the controller for autotesting requests.
+// It encapsulates logging and access to the OpenAI service.
+type AutotesterController struct {
+	config            *config.Config
+	logger            *slog.Logger
+	validationService service.ValidatePrompt
+	generationService service.GeneratePrompt
+	saveLocalService  service.TestcaseLocalStorageService
+}
+
+// NewAutotesterController creates a new AutotesterController.
+// Returns an initialized controller or an error.
+func NewAutotesterController(
+	logger *slog.Logger,
+	config *config.Config,
+	validationService service.ValidatePrompt,
+	generationService service.GeneratePrompt,
+	saveLocalService service.TestcaseLocalStorageService,
+) (*AutotesterController, error) {
+	if err := assert.NotNil(logger, config, validationService, generationService, saveLocalService); err != nil {
+		return nil, err
+	}
+
+	return &AutotesterController{
+		logger:            logger,
+		config:            config,
+		validationService: validationService,
+		generationService: generationService,
+		saveLocalService:  saveLocalService,
+	}, nil
+}
