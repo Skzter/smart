cf37b912a6e9b247ae7930686141966911773e06
Toni Nguyen <toni.nguyen@stud.htwk-leipzig.de>
2025-11-27 10:26:34 +0100

    (feat#99): add Sooner for clicking New Chat Button, fixing Sidebar Icons to fit right, New created chat will showed in Sidebar, add textfield area for chat Luca Schneider <luca.schneider@stud.htwk-leipzig.de>

--- web/src/lib/components/ui/sidebar/sidebar-provider.svelte
+++ web/src/lib/components/ui/sidebar/sidebar-provider.svelte
@@ -40,7 +40,7 @@
 <Tooltip.Provider delayDuration={0}>
 	<div
 		data-slot="sidebar-wrapper"
-		style="--sidebar-width: {SIDEBAR_WIDTH}; --sidebar-width-icon: {SIDEBAR_WIDTH_ICON}; {style}"
+		style="--sidebar-width: {SIDEBAR_WIDTH}; --sidebar-width-icon: 5rem; {style}"
 		class={cn(
 			"group/sidebar-wrapper has-data-[variant=inset]:bg-sidebar flex min-h-svh w-full",
 			className
