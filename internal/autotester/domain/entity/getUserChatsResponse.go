1e5193ea53b761887e7111d24605d5d7a74b10f0
Vincent Götze <vincent.goetze@stud.htwk-leipzig.de>
2025-11-28 19:45:06 +0100

    fix(#132): apply corrects json tags

--- internal/autotester/domain/entity/getUserChatsResponse.go
+++ internal/autotester/domain/entity/getUserChatsResponse.go
@@ -2,5 +2,5 @@ package entity
 
 // ChatSummarys is an entity for holding an array of ChatSummarys
 type ChatSummarys struct {
-	ChatSummarys []*ChatSummary `json:"chats"`
+	ChatSummarys []*ChatSummary `json:"chatSummarys"`
 }
