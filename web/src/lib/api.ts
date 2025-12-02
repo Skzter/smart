b4773912e118aa0b8e9a0848b562c235bcf477b1
Vincent Götze <vincent.goetze@stud.htwk-leipzig.de>
2025-12-02 21:39:22 +0100

    refactor(#99): renames chat component to main

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -1,64 +1,19 @@
-import axios, { AxiosError } from "axios";
-import type {
-    ApiMessage,
-    ApiChatSummary,
-    ApiChatRequest,
-    ApiChatResponse,
-    ApiSaveTestLocal,
-    ApiSaveTestLocalResponse,
-    ApiRunContainer,
-    ApiGetChatByIdResponse,
-} from "$types/api";
-import { chat, user } from "./shared.svelte";
-
-const baseURL = "http://localhost:8081/api/v1/";
+import axios from "axios";
+const baseURL = "/api/v1/";
 
 /**
  * Fetches data from the api and returns the data for the chat
  * @param params: parameters for api
  * @param url: url for api
  */
-export async function generatePrompt(
-    request: ApiChatRequest,
-): Promise<ApiChatResponse> {
-    try {
-        const response = await axios({
-            method: "post",
-            url: "chat",
-            baseURL: baseURL,
-            data: request,
-        });
-        return {
-            message: response.data.message as ApiMessage,
-            chatId: response.data.chatId,
-            userId: response.data.userId,
-        };
-    } catch (error) {
-        throw getErrorMessage(error);
-    }
-}
-
-/** Validates the prompt by sending it to the /validationRes endpoint
- * @param body: object containing userId, chatId, and prompt
- */
-export async function validatePrompt(
-    request: ApiChatRequest,
-): Promise<ApiChatResponse> {
-    try {
-        const response = await axios({
-            method: "post",
-            url: "/validate",
-            baseURL: "/api/v1/",
-            data: request,
-        });
-        return {
-            message: response.data.message as ApiMessage,
-            chatId: response.data.chatId,
-            userId: response.data.userId,
-        };
-    } catch (error) {
-        throw getErrorMessage(error);
-    }
+export async function getChatResponse(params: object, url: string) {
+    const response = await axios({
+        method: "post",
+        url: url,
+        baseURL: baseURL,
+        data: params,
+    });
+    return response;
 }
 
 /**
@@ -66,98 +21,44 @@ export async function validatePrompt(
  * @param params: parameters for api
  * @param url: url for api
  */
-export async function getChats(): Promise<ApiChatSummary[]> {
-    try {
-        const response = await axios({
-            method: "get",
-            url: `/chats`,
-            baseURL: baseURL,
-        });
-        return response.data.chatSummarys;
-    } catch (error) {
-        throw getErrorMessage(error);
-    }
+export async function getUserInfo(params: object, url: string) {
+    const response = await axios({
+        method: "post",
+        url: url,
+        baseURL: baseURL,
+        data: params,
+    });
+    return response;
 }
 
-export async function getTemplate(): Promise<string> {
-    try {
-        const response = await axios({
-            method: "get",
-            url: "template",
-            baseURL: baseURL,
-        });
-        return response.data.template;
-    } catch (error) {
-        throw getErrorMessage(error);
-    }
+export async function getTemplate(url: string) {
+    const response = await axios({
+        method: "get",
+        url: url,
+        baseURL: baseURL,
+    });
+    return response;
 }
 
 export async function saveTestLocal(
-    request: ApiSaveTestLocal,
-): Promise<ApiSaveTestLocalResponse> {
-    try {
-        const response = await axios({
-            method: "post",
-            url: "saveLocal",
-            baseURL: baseURL,
-            data: request,
-        });
-        return {
-            testcaseId: response.data.testcaseId,
-            action: response.data.action,
-        };
-    } catch (error) {
-        throw getErrorMessage(error);
-    }
-}
-
-// must block exectution until test is done
-export async function runContainer(request: ApiRunContainer): Promise<void> {
-    await axios({
+    params: object,
+    url: string = "/saveLocal",
+) {
+    const response = await axios({
         method: "post",
-        url: "run",
-        baseURL,
-        data: request,
+        url,
+        baseURL: baseURL,
+        data: params,
     });
+    return response;
 }
 
-export async function getChatById(): Promise<ApiGetChatByIdResponse> {
-    try {
-        const response = await axios({
-            method: "get",
-            url: `/chats/${chat.id}`,
-            baseURL: baseURL,
-        });
-        return response.data;
-    } catch (err) {
-        throw getErrorMessage(err);
-    }
-}
-
-export async function deleteLocalTest(testcaseId: string): Promise<string> {
-    try {
-        const response = await axios({
-            method: "delete",
-            baseURL: baseURL,
-            url: "/deleteLocal",
-            params: {
-                testcaseId,
-                chatId: chat.id,
-                userId: user.id,
-            },
-        });
-        return response.data;
-    } catch (err) {
-        throw getErrorMessage(err);
-    }
-}
-
-function getErrorMessage(error: unknown): Error {
-    if (error instanceof AxiosError) {
-        return new Error(error.response?.data.message ?? error.message);
-    } else {
-        return new Error(
-            "no axios error returned - something went horribly wrong",
-        );
-    }
+export async function runContainer(params: object, url: string = "/run") {
+    const response = await axios({
+        method: "post",
+        url,
+        baseURL: baseURL,
+        data: params,
+    });
+    return response;
 }
