e701c0f4aa3790d616b34bddd61f05c0dd9e3310
Vincent Götze <vincent.goetze@stud.htwk-leipzig.de>
2025-12-02 20:58:01 +0100

    refactor(#99): footer own component

--- web/src/lib/components/Chat.svelte
+++ web/src/lib/components/Chat.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
-    import { Bot, Plus, Send } from "@lucide/svelte";
-    import Button from "./ui/button/button.svelte";
-    import * as ButtonGroup from "$lib/components/ui/button-group";
-    import * as InputGroup from "$lib/components/ui/input-group";
+    import { Bot } from "@lucide/svelte";
     import type { Message } from "src/types/message";
     import UserMessage from "./UserMessage.svelte";
     import BotMessage from "./BotMessage.svelte";
+    import Footer from "./Footer.svelte";
 
     let isLoading = $state(false);
     let messages = $state<Message[]>([]);
@@ -43,13 +41,13 @@
     });
 </script>
 
+<!-- Hier vllt das ganze nochmal in Component der Chat heißt und die hier in Main umbenennen ig -->
 <div class="flex flex-1 flex-col gap-4 p-4 pt-0 h-full">
     <div class="flex-1 flex items-start justify-center min-h-0">
         <div
             bind:this={container}
             class="w-full max-w-6xl bg-muted/50 h-full rounded-xl md:min-h-min overflow-auto p-6 min-h-0 flex flex-col"
         >
-            <!-- CHAT MESSAGES -->
             {#if messages.length === 0}
                 <div
                     class="flex items-center justify-center flex-1 text-muted-foreground"
@@ -70,7 +68,7 @@
                             />
                         {/if}
                     {/each}
-                    <!-- Loading indicator -->
+                    <!-- Loading indicator, auch eigener component und vllt auf vorgefertigte component zurück greifen -->
                     {#if isLoading}
                         <div class="flex justify-start gap-2 items-start">
                             <div
@@ -104,39 +102,4 @@
     </div>
 </div>
 
-<div class="p-4 pt-0 sticky bottom-0 bg-background z-10">
-    <div class="flex items-center justify-center w-full">
-        <div class="w-full max-w-6xl">
-            <ButtonGroup.Root
-                class="[--radius:1rem] w-full flex items-center justify-center gap-3"
-            >
-                <ButtonGroup.Root class="shrink-0">
-                    <Button variant="outline" size="icon" class="w-10 h-10">
-                        <Plus />
-                    </Button>
-                </ButtonGroup.Root>
-                <ButtonGroup.Root class="flex-1">
-                    <div class="flex w-full items-center gap-2">
-                        <InputGroup.Root class="w-full">
-                            <InputGroup.Textarea
-                                bind:value={input}
-                                class="w-full resize-none min-h-11"
-                                placeholder="Send a message..."
-                                rows={1}
-                                disabled={isLoading}
-                            />
-                        </InputGroup.Root>
-                        <Button
-                            variant="default"
-                            size="icon"
-                            class="w-10 h-10"
-                            disabled={isLoading}
-                        >
-                            <Send />
-                        </Button>
-                    </div>
-                </ButtonGroup.Root>
-            </ButtonGroup.Root>
-        </div>
-    </div>
-</div>
+<Footer bind:input bind:isLoading />
--- web/src/lib/components/Footer.svelte
+++ web/src/lib/components/Footer.svelte
@@ -1,69 +1,44 @@
 <script lang="ts">
-    import { generatePrompt, validatePrompt } from "$lib/api";
+    import Button from "./ui/button/button.svelte";
     import * as ButtonGroup from "$lib/components/ui/button-group";
-    import { chat, messages, user } from "$lib/shared.svelte";
-    import type { ApiChatRequest } from "$types/api";
-    import Prompt from "./Prompt.svelte";
-    import SendButton from "./SendButton.svelte";
+    import * as InputGroup from "$lib/components/ui/input-group";
+    import { Send } from "@lucide/svelte";
 
-    let input = $state("");
-
-    async function onclick() {
-        if (!user.id) {
-            console.error("User is not authenticated.");
-            return;
-        }
-
-        const userQuestion = input.trim();
-        input = "";
-        messages.push({ t: "user", Message: userQuestion });
-        chat.isLoading = true;
-        let paramsChatRequest: ApiChatRequest = {
-            prompt: userQuestion,
-            userId: user.id,
-            chatId: chat.id,
-        };
-
-        let valid: boolean;
-
-        try {
-            const validationAnswer = await validatePrompt(paramsChatRequest);
-            valid = validationAnswer.message.body == "";
-            messages.push({
-                t: "validation",
-                Message: valid
-                    ? "Prompt ist Valide"
-                    : validationAnswer.message.body,
-            });
-            chat.id = validationAnswer.chatId;
-            paramsChatRequest.chatId = chat.id;
-
-            if (valid) {
-                const generationAnswer =
-                    await generatePrompt(paramsChatRequest);
-                messages.push({
-                    t: "generation",
-                    Message: generationAnswer.message.body,
-                });
-            }
-        } catch (err: unknown) {
-            messages.push({ t: "error", Message: (err as Error).message });
-        } finally {
-            chat.isLoading = false;
-        }
-    }
+    let {
+        input = $bindable(),
+        isLoading = $bindable(),
+    }: {
+        input: string;
+        isLoading: boolean;
+    } = $props();
 </script>
 
+<!-- Hier nochmal zwei Components für Input und die Button fürs abschicken -->
 <div class="p-4 pt-0 sticky bottom-0 bg-background z-10">
     <div class="flex items-center justify-center w-full">
         <div class="w-full max-w-6xl">
             <ButtonGroup.Root
-                class="[--radius:1rem] w-full flex items-center justify-center gap-3 "
+                class="[--radius:1rem] w-full flex items-center justify-center gap-3"
             >
                 <ButtonGroup.Root class="flex-1">
                     <div class="flex w-full items-center gap-2">
-                        <Prompt {onclick} bind:input />
-                        <SendButton {onclick} bind:input />
+                        <InputGroup.Root class="w-full">
+                            <InputGroup.Textarea
+                                bind:value={input}
+                                class="w-full resize-none min-h-11"
+                                placeholder="Send a message..."
+                                rows={1}
+                                disabled={isLoading}
+                            />
+                        </InputGroup.Root>
+                        <Button
+                            variant="default"
+                            size="icon"
+                            class="w-10 h-10"
+                            disabled={isLoading}
+                        >
+                            <Send />
+                        </Button>
                     </div>
                 </ButtonGroup.Root>
             </ButtonGroup.Root>
