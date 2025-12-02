a7eb8e05e75ad79ae45595fb1b45297febea6391
Vincent Götze <vincent.goetze@stud.htwk-leipzig.de>
2025-12-02 20:58:01 +0100

    refactor(#99): footer own component

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -4,20 +4,16 @@
     import Chat from "$lib/components/Chat.svelte";
 </script>
 
-<div class="test">
-    <div>
-        <Sidebar.Provider>
-            <AppSidebar />
-            <Sidebar.Inset>
-                <header
-                    class="flex justify-between h-16 shrink-0 items-center gap-2 border-b px-4"
-                >
-                    <p>CHECK24</p>
-                    <p>Playwright Test AI</p>
-                </header>
-                <Chat />
-                <!-- In inset gehört die Main Component -->
-            </Sidebar.Inset>
-        </Sidebar.Provider>
-    </div>
-</div>
+<Sidebar.Provider>
+    <AppSidebar />
+    <Sidebar.Inset>
+        <header
+            class="flex justify-between h-16 shrink-0 items-center gap-2 border-b px-4"
+        >
+            <p>CHECK24</p>
+            <p>Playwright Test AI</p>
+        </header>
+        <Chat />
+        <!-- In inset gehört die Main Component -->
+    </Sidebar.Inset>
+</Sidebar.Provider>
---
+++ web/src/lib/components/Footer.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+    import Button from "./ui/button/button.svelte";
+    import * as ButtonGroup from "$lib/components/ui/button-group";
+    import * as InputGroup from "$lib/components/ui/input-group";
+    import { Send } from "@lucide/svelte";
+
+    let {
+        input = $bindable(),
+        isLoading = $bindable(),
+    }: {
+        input: string;
+        isLoading: boolean;
+    } = $props();
+</script>
+
+<!-- Hier nochmal zwei Components für Input und die Button fürs abschicken -->
+<div class="p-4 pt-0 sticky bottom-0 bg-background z-10">
+    <div class="flex items-center justify-center w-full">
+        <div class="w-full max-w-6xl">
+            <ButtonGroup.Root
+                class="[--radius:1rem] w-full flex items-center justify-center gap-3"
+            >
+                <ButtonGroup.Root class="flex-1">
+                    <div class="flex w-full items-center gap-2">
+                        <InputGroup.Root class="w-full">
+                            <InputGroup.Textarea
+                                bind:value={input}
+                                class="w-full resize-none min-h-11"
+                                placeholder="Send a message..."
+                                rows={1}
+                                disabled={isLoading}
+                            />
+                        </InputGroup.Root>
+                        <Button
+                            variant="default"
+                            size="icon"
+                            class="w-10 h-10"
+                            disabled={isLoading}
+                        >
+                            <Send />
+                        </Button>
+                    </div>
+                </ButtonGroup.Root>
+            </ButtonGroup.Root>
+        </div>
+    </div>
+</div>
