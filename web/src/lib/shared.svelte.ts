6e7048594e8c8a08b5ec921bcedf2b66bc1ef7a4
Vincent Götze <vincent.goetze@stud.htwk-leipzig.de>
2025-12-03 17:41:40 +0100

    feat(#99): adds Code Component from FrontendV1 with correct dark/light mode switch

--- web/src/lib/shared.svelte.ts
+++ web/src/lib/shared.svelte.ts
@@ -1,5 +1,7 @@
 import type { Message } from "$types/message";
 
+export const messages: Message[] = $state([]);
+
 export const user = $state({
     id: "",
 });
@@ -8,5 +10,3 @@ export const chat = $state({
     id: "",
     isLoading: false,
 });
-
-export const messages: Message[] = $state([]);
