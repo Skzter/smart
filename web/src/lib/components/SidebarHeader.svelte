55a44c449152ed8ba8bc94e8a17e753b37392257
Luca Schneider <luca.schneider@stud.htwk-leipzig.de>
2025-12-04 20:56:11 +0100

    feat(#99): setup the sidebar header and added newchat + timefilter button

---
+++ web/src/lib/components/SidebarHeader.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import * as Sidebar from "$lib/components/ui/sidebar";
+    import NewChatButton from "./NewChatButton.svelte";
+    import TimeFilter from "./TimeFilterButton.svelte";
+</script>
+
+<Sidebar.Header class="border-b">
+    <div class="flex flex-col gap-2 p-2">
+        <NewChatButton />
+        <TimeFilter />
+    </div>
+</Sidebar.Header>
