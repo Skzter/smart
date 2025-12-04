d98107d89bddcd6f6c09b11145d74b65f61386fe
Bruno Bittner <bruno.bittner@stud.htwk-leipzig.de>
2025-12-04 18:21:13 +0100

    feat(#99): adds groupings to sidebar

--- web/src/lib/components/Group.svelte
+++ web/src/lib/components/Group.svelte
@@ -1,52 +1,39 @@
 <script lang="ts">
     import * as Sidebar from "$lib/components/ui/sidebar/index.js";
-    import { getUserChats } from "$lib/api";
     import type { ApiChatSummary } from "$types/api";
-    import { user } from "$lib/shared.svelte";
     import { LucideMessageSquare } from "@lucide/svelte";
 
-    let items = $state<ApiChatSummary[]>([]);
-    console.log(user.id);
-
-    (async () => {
-        if (user.id == undefined) {
-            return;
-        }
-        try {
-            items = (await getUserChats(user.id)) as ApiChatSummary[];
-        } catch (error) {
-            console.log(error);
-        }
-    })();
+    let {
+        group = $bindable(),
+    }: { group: { label: String; summaries: ApiChatSummary[] } } = $props();
 </script>
 
 <Sidebar.Group>
-    <Sidebar.GroupLabel>Application</Sidebar.GroupLabel>
+    <Sidebar.GroupLabel class="uppercase">{group.label}</Sidebar.GroupLabel>
     <Sidebar.GroupContent>
         <Sidebar.Menu>
-            {#each items as item}
+            {#each group.summaries as chat}
                 <Sidebar.MenuItem>
                     <Sidebar.MenuButton
-                        class="h-full py-1"
+                        class="h-full py-1 m-1"
                         onclick={() => {
                             console.log(
                                 "changing to: ",
-                                item.userId,
-                                item.chatId,
+                                chat.userId,
+                                chat.chatId,
                             );
                         }}
                     >
                         <LucideMessageSquare class="mr-1" />
-                        <div>
+                        <div class="h-full flex flex-col justify-between">
                             <p class="text-sm text-gray-900 dark:text-gray-200">
-                                {item.title == "" ? "Neuer Chat" : item.title}
+                                {chat.title == "" ? "Neuer Chat" : chat.title}
                             </p>
                             <span
                                 class="font-mono text-xs text-gray-500 dark:text-gray-400"
-                                >{item.updatedAt.substring(11, 16)}</span
+                                >{chat.updatedAt.substring(11, 16)}</span
                             >
                         </div>
-                        <span> </span>
                     </Sidebar.MenuButton>
                 </Sidebar.MenuItem>
             {/each}
--- web/src/lib/components/app-sidebar.svelte
+++ web/src/lib/components/app-sidebar.svelte
@@ -1,10 +1,72 @@
 <script lang="ts">
     import * as Sidebar from "$lib/components/ui/sidebar/index.js";
     import Group from "./Group.svelte";
+    import { getUserChats } from "$lib/api";
+    import type { ApiChatSummary } from "$types/api";
+    import { user } from "$lib/shared.svelte";
+
+    let groupState = $state<{ label: String; summaries: ApiChatSummary[] }[]>(
+        [],
+    );
+    let groups = new Map<string, ApiChatSummary[]>();
+    function add(group: string, item: ApiChatSummary) {
+        if (groups.has(group)) {
+            groups.get(group)?.push(item);
+        } else {
+            groups.set(group, [item]);
+        }
+    }
+
+    let items: ApiChatSummary[] | undefined;
+    (async () => {
+        if (user.id == undefined) {
+            return;
+        }
+        try {
+            items = (await getUserChats(user.id)) as ApiChatSummary[];
+        } catch (error) {
+            console.log(error);
+        }
+
+        console.log(items);
+        items?.forEach((item) => {
+            let now = new Date();
+            let time = new Date(Date.parse(item.updatedAt));
+            if (
+                now.getFullYear === time.getFullYear &&
+                now.getMonth === time.getMonth
+            ) {
+                let days = now.getDate() - time.getDate();
+                switch (days) {
+                    case 0:
+                        add("Heute", item);
+                        return;
+                    case 1:
+                        add("Gestern", item);
+                        return;
+                }
+                if (days <= 7) {
+                    add("letzte Woche", item);
+                    return;
+                }
+                add("letzten Monat", item);
+                return;
+            }
+            add("früher", item);
+        });
+
+        let g: { label: String; summaries: ApiChatSummary[] }[] = [];
+        groups.forEach((value, key) => {
+            g.push({ label: key, summaries: value });
+        });
+        groupState = g;
+    })();
 </script>
 
 <Sidebar.Root>
     <Sidebar.Content>
-        <Group></Group>
+        {#each groupState as g, index}
+            <Group bind:group={groupState[index]}></Group>
+        {/each}
     </Sidebar.Content>
 </Sidebar.Root>
