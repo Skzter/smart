78c0f1b9f2c769744c92b9248945c4ca85d22b91
Luca Schneider <luca.schneider@stud.htwk-leipzig.de>
2025-12-08 20:49:34 +0100

    fix(#99): fully configured vite config to support testing

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,16 +1,46 @@
+/// <reference types="vitest/config" />
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import path from "path";
 import mkcert from "vite-plugin-mkcert";
+import { svelteTesting } from "@testing-library/svelte/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [tailwindcss(), svelte(), mkcert()],
+    server: {
+        https: true,
+    },
+    plugins: [tailwindcss(), svelte(), mkcert(), svelteTesting()],
     resolve: {
         alias: {
             $lib: path.resolve("./src/lib"),
             $types: path.resolve("./src/types"),
+        },
+    },
+    test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: "./src/setupTests.ts",
+        include: ["tests/unit/*.test.ts", "tests/components/*.test.ts"],
+        reporters: ["junit", "default"],
+        outputFile: "coverage/junit.xml",
+        coverage: {
+            provider: "v8",
+            reporter: ["text", "html", "cobertura", "lcov"],
+            include: ["src/{lib,components}/**/*.{ts,js,svelte}"],
+            exclude: [
+                "src/**/*.test.{ts}",
+                "src/**/*.d.ts",
+                "src/lib/authService.ts",
+            ],
+            reportsDirectory: "./coverage",
+            thresholds: {
+                statements: 80,
+                branches: 80,
+                functions: 80,
+                lines: 80,
+            },
         },
     },
 });
