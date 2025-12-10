11b4e4d050ff4e762b6320b1ea76645d5af87c30
Lucas Stramke <lucas.stramke@stud.htwk-leipzig.de>
2025-12-10 11:18:17 +0100

    chore(#137): transform yaml config in pkl config

--- internal/mcp/domain/config/config.go
+++ internal/mcp/domain/config/config.go
@@ -1,55 +1,18 @@
 package config
 
-/* import (
-	"fmt"
-	"os"
+import (
+	"github.com/apple/pkl-go/pkl"
 
-	"gopkg.in/yaml.v3"
+	"gitlab.dit.htwk-leipzig.de/projekt2025-w-llm-unterstuetztes-autotesting-fuer-moderne-web-frontends/smart/internal/build"
 )
 
-type Config struct {
-	Name    string         `yaml:"name"`
-	Version string         `yaml:"version"`
-	Schema  string         `yaml:"schema"`
-	Models  []ModelConfig  `yaml:"models"`
-	Context ContextConfig  `yaml:"context"`
-	Servers []ServerConfig `yaml:"mcpServers"`
-}
-
-type ModelConfig struct {
-	Name     string   `yaml:"name"`
-	Provider string   `yaml:"provider"`
-	Model    string   `yaml:"model"`
-	Roles    []string `yaml:"roles"`
-
-	DefaultCompletionOptions struct {
-		MaxTokens int `yaml:"maxTokens"`
-	} `yaml:"defaultCompletionOptions"`
-}
-
-type ContextConfig struct {
-	Provider string `yaml:"provider"`
-}
-
-type ServerConfig struct {
-	Name    string            `yaml:"name"`
-	Command string            `yaml:"command"`
-	Args    []string          `yaml:"args"`
-	Cwd     string            `yaml:"cwd"`
-	Env     map[string]string `yaml:"env"`
-}
-
-func LoadConfig(path string) (*Config, error) {
-	data, err := os.ReadFile(path)
-	if err != nil {
-		return nil, fmt.Errorf("cannot read config file: %w", err)
-	}
-
+// LoadAppConfig loads application config
+func LoadAppConfig() (*Config, error) {
 	var cfg Config
-	if err := yaml.Unmarshal(data, &cfg); err != nil {
-		return nil, fmt.Errorf("cannot parse YAML: %w", err)
+	bytes, err := build.SuproxyEmbedConfigs.ReadFile("configs/suproxy.msgpack")
+	if err != nil {
+		return nil, err
 	}
-
-	return &cfg, nil
+	err = pkl.Unmarshal(bytes, &cfg)
+	return &cfg, err
 }
-*/
