1c0fa9d6f25367ff78d88dc12715c18c411bd45a
Toni Nguyen <toni.nguyen@stud.htwk-leipzig.de>
2025-12-11 14:47:08 +0100

    feat(#99): implement dropdown menu components without function

--- web/src/lib/components/SidebarHeader.svelte
+++ web/src/lib/components/SidebarHeader.svelte
@@ -2,11 +2,13 @@
     import * as Sidebar from "$lib/components/ui/sidebar";
     import NewChatButton from "./NewChatButton.svelte";
     import TimeFilter from "./TimeFilter.svelte";
+    import ChatFilter from "./ChatFilter.svelte";
 </script>
 
 <Sidebar.Header class="border-b">
     <div class="flex flex-col gap-2 p-2">
         <NewChatButton />
         <TimeFilter />
+        <ChatFilter />
     </div>
 </Sidebar.Header>
