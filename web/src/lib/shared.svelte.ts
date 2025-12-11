25b6de01ae9a69d0fb6d26fbf9650f00597db9e5
Vincent Götze <vincent.goetze@stud.htwk-leipzig.de>
2025-12-11 13:47:42 +0100

    feat(#99): adds auth + login button

--- web/src/lib/shared.svelte.ts
+++ web/src/lib/shared.svelte.ts
@@ -4,7 +4,7 @@ import type { DateRange } from "bits-ui";
 export const messages: Message[] = $state([]);
 
 export const user = $state({
-    id: "",
+    id: "" as string | undefined,
 });
 
 export const chat = $state({
